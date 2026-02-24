cdf77ca526b70da169d83ca0182e411bb81799bd
Joitru <vinchalgei@gmail.com>
2026-02-24 10:34:21 +0300

    Add files via upload

--- js/maim.js
+++ js/maim.js
@@ -13,3 +13,12 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+    const cards = document.querySelectorAll('.top-card');
+    
+    cards.forEach(card => {
+        card.addEventListener('click', function() {
+            window.location.href = 'card.html';
+        });
+        
+        card.style.cursor = 'pointer';
+    });
